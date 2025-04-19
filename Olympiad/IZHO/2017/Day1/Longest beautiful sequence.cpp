74d35928654279be114bd960f11857250c9893e4
SavicStefan <50296686+SavicStefan@users.noreply.github.com>
2025-04-19 16:27:49 +0200

    Update Longest beautiful sequence.cpp

--- Olympiad/IZHO/2017/Day1/Longest beautiful sequence.cpp
+++ Olympiad/IZHO/2017/Day1/Longest beautiful sequence.cpp
@@ -35,41 +35,84 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 
 const int mod = 1000000007;
 const int inf = 1e9 + 5;
-const int mxN = 1000005; 
+const int mxN = 100005; 
 
 int n;
-int niz[mxN];
+int A[mxN];
+int K[mxN];
+
+const int L = 10;
+int dp[(1 << L)][(1 << L)][11];
+int ko[(1 << L)][(1 << L)][11];
+
+int par[mxN];
 
 int main(){
     cin.tie(0)->sync_with_stdio(0);
-
+    
     cin >> n;
-    ff(i,1,n)cin >> niz[i];
+    ff(i,1,n)cin >> A[i];
+    ff(i,1,n)cin >> K[i];
 
-    set<int> s; int rez = 0;
+    int rez = 0, g = 0;
     ff(i,1,n){
-        rez += 1; 
+        int ad = A[i] & ((1 << L) - 1);
+        int ag = A[i] >> L;
 
-        auto a = s.ub(niz[i]);
-        int R = (a == s.end() ? inf : *a);
+        int mx = 1, gde = 0;
+        ff(mask,0,(1 << L) - 1){
+            ff(j,0,10){
+                if(j + __builtin_popcount(mask & ad) == K[i]){
+                    
+                    if(dp[mask][ag][j] + 1 > mx){
+                        mx = dp[mask][ag][j] + 1;
+                        gde = ko[mask][ag][j];
+                    }
 
-        int j = i;
-        while(j + 1 <= n && niz[j + 1] >= niz[j] && niz[j + 1] <= R){
-            j += 1;
+                }
+            }
         }
 
-        ff(l,i,j)s.insert(niz[l]);
-        i = j;
+        par[i] = gde;
+        if(mx > rez){
+            rez = mx;
+            g = i;
+        }
+
+        ff(mask,0,(1 << L) - 1){
+            int k = __builtin_popcount(ag & mask);
+
+            if(dp[ad][mask][k] < mx){
+                dp[ad][mask][k] = mx;
+                ko[ad][mask][k] = i;
+            }
+
+        }        
 
     }
 
-    cout << rez << '\n';
+    vector<int> res;
+    for(int i = g; i != 0; i = par[i])res.pb(i);
+
+    cout << sz(res) << '\n'; reverse(all(res));
+    for(auto c : res)cout << c << " ";
+    cout << '\n';
 
     return 0;
 }
 /*
 
+4
+1 2 3 4 
+10 0 1 0
 
+2
+8 9
+20 0
+
+5
+5 3 5 3 5
+10 1 20 1 20
 
 // probati bojenje sahovski
 */
